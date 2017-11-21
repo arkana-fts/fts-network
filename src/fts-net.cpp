2075ca89691affd266f4a619f58109d5ca05af69
xkbeyer <xkbeyer@gmail.com>
2017-11-21 19:50:20 +0100

    On win32 load the socket subsystem.

--- src/fts-net.cpp
+++ src/fts-net.cpp
@@ -1,9 +1,22 @@
+#include <string>
 #include "fts-net.h"
 #include "Logger.h"
+#if defined(_WIN32)
+#  pragma comment(lib, "Ws2_32.lib")
+#  include<winsock2.h>
+#endif
 
 bool FTS::NetworkLibInit( int dbgLevel, std::ostream * out )
 {
     Logger::DbgLevel( dbgLevel );
     Logger::LogFile(out);
+
+#if defined(_WIN32)
+    // And the windows sockets.
+    WSADATA wsaData;
+    if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+        FTSMSGDBG("Winsock version {1}.{2} initialized ", 2, std::to_string(LOBYTE(wsaData.wVersion)), std::to_string(HIBYTE(wsaData.wVersion)));
+#endif
+
     return true;
 }
